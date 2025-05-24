835cd7b04e1429c43e6ac402951efb2e7e78d708
yilee <930617673@qq.com>
2025-05-24 16:04:55 +0800

    tjupt签到答案使用大模型

--- plugins.v2/autosignin/sites/tjupt.py
+++ plugins.v2/autosignin/sites/tjupt.py
@@ -1,3 +1,4 @@
+import base64
 import json
 import os
 import time
@@ -10,6 +11,7 @@ from ruamel.yaml import CommentedMap
 
 from app.core.config import settings
 from app.log import logger
+from app.plugins.autosignin.openai import OpenAi
 from app.plugins.autosignin.sites import _ISiteSigninHandler
 from app.utils.http import RequestUtils
 from app.utils.string import StringUtils
@@ -36,6 +38,23 @@ class Tjupt(_ISiteSigninHandler):
     # 存储正确的答案，后续可直接查
     _answer_path = settings.TEMP_PATH / "signin/"
     _answer_file = _answer_path / "tjupt.json"
+
+    def __init__(self):
+        self.openai = None
+        chatgpt = self.get_config("ChatGPT")
+        if not chatgpt:
+            logger.error(f"ChatGPT插件未启用")
+            return
+        self._openai_key = chatgpt and chatgpt.get("openai_key")
+        self._openai_url = chatgpt and chatgpt.get("openai_url")
+        self._openai_proxy = chatgpt and chatgpt.get("proxy")
+        self._openai_model = chatgpt and chatgpt.get("model")
+        if not self._openai_key:
+            logger.error(f"ChatGPT插件未启用")
+            return
+        self.openai = OpenAi(api_key=self._openai_key, api_url=self._openai_url,
+                             proxy=settings.PROXY if self._openai_proxy else None,
+                             model=self._openai_model)
 
     @classmethod
     def match(cls, url: str) -> bool:
@@ -120,6 +139,23 @@ class Tjupt(_ISiteSigninHandler):
         # value+选项
         answers = list(zip(values, options))
         logger.debug(f"获取到所有签到选项 {answers}")
+
+        if self.openai:
+            base64_img = base64.b64encode(BytesIO(captcha_img_res.content).read())
+            ret, result = self.openai.get_answer_with_img(options.join("\n"), base64_img)
+            if not ret:
+                logger.error(f"{site} 签到失败，ChatGPT未返回答案")
+            else:
+                for value, answer in answers:
+                    if result and result in answer:
+                        # 确实是答案
+                        return self.__signin(
+                            answer=value,
+                            site_cookie=site_cookie,
+                            ua=ua,
+                            proxy=proxy,
+                            site=site
+                        )
 
         # 查询已有答案
         exits_answers = {}
