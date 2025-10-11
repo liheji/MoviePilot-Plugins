78eca9c7ed05f5e96bd252f322bca62e4b01690a
yilee <930617673@qq.com>
2025-10-12 00:46:41 +0800

    feat(siteopencheck): 升级插件版本至2.2

--- plugins.v2/siteopencheck/__init__.py
+++ plugins.v2/siteopencheck/__init__.py
@@ -28,7 +28,7 @@ class SiteOpenCheck(_PluginBase):
     # 插件图标
     plugin_icon = "signin.png"
     # 插件版本
-    plugin_version = "2.1"
+    plugin_version = "2.2"
     # 插件作者
     plugin_author = "liheji"
     # 作者主页
